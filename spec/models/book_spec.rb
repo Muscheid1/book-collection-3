ccd4e8074269efbdeb75cd4480b1f785955965c6
root <root@localhost>
2025-09-10 04:32:30 +0000

    finished book collection 2

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,11 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   it 'is valid when book has title' do
-    expect(Book.new(title: 'example title')).to be_valid
+    expect(Book.new(title: 'example title', author: 'example author')).to be_valid
   end
 
   it 'is invald when book does not have title' do
     book = Book.new(title: '')
     expect(book).not_to be_valid
   end
+
+  it 'is valid when book has author' do
+    expect(Book.new(author: 'example author', title: 'example title')).to be_valid
+  end
+
+
+  it 'is invalid when book does not have author' do
+    expect(Book.new(author: '')).not_to be_valid
+  end
+
+
+
 end
